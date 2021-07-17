169afde725ba56c14084a52b7b684ba29afdc404
Travis Pinkerton <travispinkerton@hotmail.com>
2021-07-16 22:27:01 -0700

    server edit

--- index.js
+++ index.js
@@ -30,12 +30,14 @@ const { client } = require('./db');
 // connect to the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, async () => {
-  console.log(`Server is running on ${ PORT }!`);
+	
 
-  try {
-    await client.connect();
-    console.log('Database is open for business!');
-  } catch (error) {
-    console.error("Database is closed for repairs!\n", error);
-  }
+	try {
+		await client.connect();
+		console.log('Database is open for business!');
+	} catch (error) {
+		console.error('Database is closed for repairs!\n', error);
+	} finally {
+		console.log(`listening on port http://localhost:${PORT}`)
+	}
 });
